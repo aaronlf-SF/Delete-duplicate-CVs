f6ab3305cb54ed13fae25824a95ed33c970bf0d9
aaron-SF <41145580+aaronlf-SF@users.noreply.github.com>
2018-08-08 09:28:10 +0100

    Code clean-up

--- pdf_read.py
+++ pdf_read.py
@@ -17,15 +17,17 @@ def pdf_to_text(path):
     maxpages = 0
     caching = True	
     pagenos=set()
+	
     try:
         for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
             interpreter.process_page(page)
         text = retstr.getvalue()
     except:
         text = 'error'
+		
     fp.close()
     device.close()
     retstr.close()
+	
     return text
-
-#print(pdf_to_text('C:/Users/AFitzpatrick/Desktop/Programming/testing/PM-3128.pdf'))
+	
