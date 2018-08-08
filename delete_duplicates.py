30d23981509de130c7b11b6d5eac10ebf4ffec39
aaron-SF <41145580+aaronlf-SF@users.noreply.github.com>
2018-08-08 09:17:49 +0100

    Code tidy-up

--- delete_duplicates.py
+++ delete_duplicates.py
@@ -12,14 +12,18 @@ import docx_read as dxr
 import doc_read as dr
 
 
+
 #======================================================================
 
 
-PATH = 'C:/Users/AFitzpatrick/Desktop/Programming/testing/[CONSULTANT NAME]/'
+
+PATH = "C:/Users/AFitzpatrick/Desktop/Programming/testing/[CONSULTANT NAME]/"
 filesDict = {} #dictionary of the form {filename : file extension}
 
 
+
 #======================================================================
+
 
 
 def main():
@@ -30,6 +34,7 @@ def main():
 	
 	
 #======================================================================
+
 
 
 def organise_files():
@@ -44,14 +49,17 @@ def organise_files():
 			filesDict[filename] = '.pdf'
 		else:
 			print(filename,'not organised')
-		
+	
+	
 		
 #======================================================================
 
 
+
 def find_email_address(filename,text):
 	'''
-	FIND EMAIL ADDRESSES CONTAINED IN FILES	
+	FIND EMAIL ADDRESSES CONTAINED IN DOCUMENT BY
+	PARSING FOR '@' SYMBOL. 
 	'''
 	emailAddress = ''
 	if text == 'error':
@@ -67,6 +75,9 @@ def find_email_address(filename,text):
 
 
 def extract_text(filename):
+	'''
+	RETURNS THE RAW TEXT FROM THE DOCUMENT
+	'''
 	global PATH
 	if filesDict[filename] == '.docx':
 		text = dxr.docx_to_text(PATH+filename)
@@ -74,11 +85,13 @@ def extract_text(filename):
 		text = dr.doc_to_text(PATH+filename)
 	elif filesDict[filename] == '.pdf':
 		text = pr.pdf_to_text(PATH+filename)
-	#print(text,'\n',filename)
 	return text	
 	
 	
 def extract_text_on_time(file):
+	'''
+	NECESSARY DEPENDENT FUNCTION FOR EXTRACTING TEXT ON A SEPARATE THREAD
+	'''
 	pythoncom.CoInitialize()
 	global textFound,output_text
 	output_text = extract_text(file)
@@ -86,6 +99,10 @@ def extract_text_on_time(file):
 
 		
 def getText(file):
+	'''
+	RETRIEVES THE TEXT ON A SEPARATE THREAD WITH A TIMEOUT OF 45 SECONDS
+	TO ACCOUNT FOR EXCEPTIONS THE PROGRAM CAN'T CATCH
+	'''
 	global textFound,output_text
 	textFound = False
 		
@@ -97,8 +114,10 @@ def getText(file):
 		output_text = 'error'
 	return output_text
 
-		
+	
+	
 #======================================================================
+
 
 
 def delete_duplicates():
@@ -126,7 +145,7 @@ def delete_duplicates():
 				
 			elif emailAddress == 'error':
 				#remove email addresses with errors
-				print('Error encountered when processing ' + file + 'Removing.')
+				print('Error encountered when processing ' + file + '. Removing.')
 				os.remove(PATH+file)
 				uniqueCount += 1
 				totalCount += 1
@@ -147,10 +166,17 @@ def delete_duplicates():
 	print('Total number of deletions:',totalCount)
 
 	
+	
 #======================================================================
 
 
+
 def compare_and_remove_files(file,emailAddress,filenames_and_emails,text):
+	'''
+	CHECKS TO SEE IF A DOCUMENT SHOULD BE DELETED BASED ON WHETHER OR NOT 
+	ANOTHER FILE SHARES THE SAME EMAIL ADDRESS STRING, OR CONTAINS NO
+	EMAIL ADDRESS AT ALL
+	'''
 	latestYear = find_latest_year(text)
 	if file == sorted(filesDict)[-1]: # LAST FILE IN THE DIRECTORY
 		filenames_and_emails[file] = {'email':emailAddress, 'latestYear':latestYear}
@@ -164,9 +190,7 @@ def compare_and_remove_files(file,emailAddress,filenames_and_emails,text):
 				
 			safeDict = {'filename':[safeFile],'email':[email],'latestYear':[maxYear]}
 			compare_against_dataset_file(safeDict)
-		'''if file == sorted(filesDict)[-1]:
-			fileDict = {'filename':[file],'email':[emailAddress],'latestYear':[latestYear]}
-			compare_against_dataset_file(fileDict)'''	
+	
 	else:
 		fileDict = {'filename':[file],'email':[emailAddress],'latestYear':[latestYear]}
 		compare_against_dataset_file(fileDict)			
@@ -177,12 +201,11 @@ def get_max_year(filenames_and_emails):
 	for i in filenames_and_emails:
 		years.append(filenames_and_emails[i]['latestYear'])
 	return max(years)
-							
-							
+													
 							
 def keep_safe_file(filenames_and_emails,maxYear):
 	'''
-	delete all but that one in that year
+	DELETE ALL FILES BUT ONE 'SAFE FILE' FROM THAT YEAR
 	'''
 	global totalCount, uniqueCount
 	duplicate_files = [f for f in filenames_and_emails if filenames_and_emails[f]['latestYear'] == maxYear]
@@ -199,11 +222,18 @@ def keep_safe_file(filenames_and_emails,maxYear):
 		
 	return safeFile
 	
+	
 
 #======================================================================
 	
 	
+	
 def compare_against_dataset_file(safeDict):
+	'''
+	CHECK THE EXISTING DATAFRAME OF FILES AND EMAIL ADDRESSES
+	TO SEE IF IT HAS BEEN FOUND ALREADY, THEN DEAL WITH THE RESULT
+	ACCORDINGLY
+	'''
 	global PATH, uniqueCount,totalCount
 	try:
 		df = csv_to_df()
@@ -249,58 +279,37 @@ def csv_to_df():
 	return df
 
 	
+	
 #======================================================================
 
+
+
 def find_latest_year(text):
-	years = {
-			1990:['1990',"'90",'90'],
-			1991:['1991',"'91",'91'],
-			1992:['1992',"'92",'92'],
-			1993:['1993',"'93",'93'],
-			1994:['1994',"'94",'94'],
-			1995:['1995',"'95",'95'],
-			1996:['1996',"'96",'96'],
-			1997:['1997',"'97",'97'],
-			1998:['1998',"'98",'98'],
-			1999:['1999',"'99",'99'],
-			2000:['2000',"'00",'00'],
-			2001:['2001',"'01",'01'],
-			2002:['2002',"'02",'02'],
-			2003:['2003',"'03",'03'],
-			2004:['2004',"'04",'04'],
-			2005:['2005',"'05",'05'],
-			2006:['2006',"'06",'06'],
-			2007:['2007',"'07",'07'],
-			2008:['2008',"'08",'08'],
-			2009:['2009',"'09",'09'],
-			2010:['2010',"'10",'10'],
-			2011:['2011',"'11",'11'],
-			2012:['2012',"'12",'12'],
-			2013:['2013',"'13",'13'],
-			2014:['2014',"'14",'14'],
-			2015:['2015',"'15",'15'],
-			2016:['2016',"'16",'16'],
-			2017:['2017',"'17",'17'],
-			2018:['2018',"'18",'18'],
-			}
+	'''
+	LOOKS THROUGH THE TEXT TO FIND YEAR KEYWORDS TO DETERMINE
+	HOW RECENT THE FILE IS. LOOKS FOR YEARS FROM 1990 - 2018
+	'''
+	years = {}
+	
+	for year in range(1990,2019):
+		years[year] = [
+						str(year),  #e.g. 2012
+						"'" + str(year)[2:], # e.g. '12
+						str(year)[2:]  # e.g. 12
+						]
 
 	years_found = []
 	for word in text.split():
-		for year in list(years.keys()):
-			if word in years[year][:2]:
-				years_found.append(year)
-	for word in text.split('/'):
-		for year in list(years.keys()):
-			if word in years[year]:
-				years_found.append(year)
-	for word in text.split('-'):
-		for year in list(years.keys()):
-			if word in years[year]:
-				years_found.append(year)
-	for word in text.split('.'):
-		for year in list(years.keys()):
-			if word in years[year]:
-				years_found.append(year)
+		for year in years:
+			for year_format in years[year][:2]:
+				if word == year_format:
+					years_found.append(year)
+	
+	for word in ( text.split('/') + text.split('-') text.split('.') ):
+		for year_format in years[year]:
+				if word == year_format:
+					years_found.append(year)
+
 	if len(years_found) > 0:			
 		latestYear = max(years_found)
 	else:
@@ -308,9 +317,13 @@ def find_latest_year(text):
 	return latestYear
 
 	
+	
 #======================================================================	
+	
 	
 	
 if __name__ == '__main__':
 	main()
+	
+
 	
